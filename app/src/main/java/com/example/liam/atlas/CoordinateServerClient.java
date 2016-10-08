ea98d0e058373b023ec1e32a93ef7265e9ea3767
Sahil <slymishra@gmail.com>
2016-10-08 16:01:14 -0400

    finished the server client

--- app/src/main/java/com/example/liam/atlas/CoordinateServerClient.java
+++ app/src/main/java/com/example/liam/atlas/CoordinateServerClient.java
@@ -13,23 +13,48 @@ import java.util.ArrayList;
  */
 public class CoordinateServerClient {
     private static final String serverHostname = new String ("127.0.0.1");
+    private static final String SendCoordinateMessage = "request";
 
 
-    public static boolean SendMessage(Coordinate coordinate) {
-
-        return false;
-    }
-
-    public static ArrayList<Coordinate> RecieveData() throws IOException {
-
-        System.out.println ("Attemping to connect to host " +
-                serverHostname + " on port 10008.");
+    public static boolean SendMessage(Coordinate coordinate) throws IOException {
 
         Socket echoSocket = null;
+        PrintWriter out = null;
         BufferedReader in = null;
 
         try {
             echoSocket = new Socket(serverHostname, 10008);
+            out = new PrintWriter(echoSocket.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(
+                    echoSocket.getInputStream()));
+        } catch (UnknownHostException e) {
+            System.err.println("Don't know about host: " + serverHostname);
+            return false;
+        } catch (IOException e) {
+            System.err.println("Couldn't get I/O for "
+                    + "the connection to: " + serverHostname);
+            return false;
+        }
+
+        out.println(SendCoordinateMessage);
+        String serverMessage=in.readLine();
+        System.out.println(serverMessage);
+        out.close();
+        in.close();
+        echoSocket.close();
+        return true;
+    }
+
+    public static ArrayList<Coordinate> RecieveData() throws IOException {
+
+
+        Socket echoSocket = null;
+        PrintWriter out = null;
+        BufferedReader in = null;
+
+        try {
+            echoSocket = new Socket(serverHostname, 10008);
+            out = new PrintWriter(echoSocket.getOutputStream(), true);
             in = new BufferedReader(new InputStreamReader(
                     echoSocket.getInputStream()));
         } catch (UnknownHostException e) {
@@ -41,16 +66,18 @@ public class CoordinateServerClient {
             return null;
         }
 
-        String serverMessage = in.readLine();
+        out.println(SendCoordinateMessage);
+        String serverMessage=in.readLine();
+        out.close();
         in.close();
         echoSocket.close();
 
-        if (serverMessage.equals(null))
+        if (serverMessage.equals("empty"))
             return null;
 
         ArrayList<Coordinate> Coordinates = new ArrayList<Coordinate>();
         for (String coordinateString :
-             serverMessage.split("~")) {
+                serverMessage.split("~")) {
             String[] coordinateComponents = coordinateString.split(",");
             Coordinate coordinate = new Coordinate(Double.parseDouble(coordinateComponents[0]),Double.parseDouble(coordinateComponents[1]),Utilities.StringToBitMap(coordinateComponents[2]));
             Coordinates.add(coordinate);
