d6a80eb42d66ba665f14b2b81e8c925a3b51d95d
Ryan Jerue <ryan@jerue.org>
2016-10-08 02:11:04 -0400

    Button is wired

---
+++ app/src/main/java/com/example/liam/atlas/Camera.java
@@ -0,0 +1,10 @@
+package com.example.liam.atlas;
+
+import android.app.Activity;
+
+/**
+ * Created by Ryan on 10/8/2016.
+ */
+
+public class Camera extends Activity {
+}
--- app/src/main/java/com/example/liam/atlas/MapsActivity.java
+++ app/src/main/java/com/example/liam/atlas/MapsActivity.java
@@ -9,10 +9,17 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
+import android.view.View;
+import android.widget.Button;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private Button cameraButton;
+    private Camera camera;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,5 +49,33 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+    public void cameraButtonClick(View view){
+        if(checkForCamera(getApplicationContext())){
+
+        }
+    }
+
+
+    public static Camera getCameraInstance(){
+        Camera c = null;
+        try {
+            c = Camera.open(); // attempt to get a Camera instance
+        }
+        catch (Exception e){
+            // Camera is not available (in use or does not exist)
+        }
+        return c; // returns null if camera is unavailable
+    }
+
+    private static boolean checkForCamera(Context context) {
+        if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)){
+            // this device has a camera
+            return true;
+        } else {
+            // no camera on this device
+            return false;
+        }
     }
 }
